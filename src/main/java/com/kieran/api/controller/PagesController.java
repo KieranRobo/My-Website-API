d1fc453eb2ec1c91d1deded4e413a4516a8626b2
Kieran Robertson <kieran.robertson.2013@uni.strath.ac.uk>
2018-12-02 13:22:52 +0000

    Getting pages returns JSON.

--- src/main/java/com/kieran/api/controller/PagesController.java
+++ src/main/java/com/kieran/api/controller/PagesController.java
@@ -1,7 +1,9 @@
 package com.kieran.api.controller;
 
+import com.google.gson.Gson;
 import com.kieran.api.dao.PagesDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,11 @@ public class PagesController {
     @Autowired
     private PagesDao dao;
 
+    private Gson gson = new Gson();
+
     @RequestMapping(value = "/pages/all", method = RequestMethod.GET)
     public String test() {
         List<Map<String, Object>> allPages = dao.queryForAllPages();
-        return "Found " + allPages.size() + " pages.";
+        return gson.toJson(allPages);
     }
 }
