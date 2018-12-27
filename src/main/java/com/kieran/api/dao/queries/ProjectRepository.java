07a865433ec2753e8bc53640b5f28078ea805700
612032222 <kieran.robertson@bt.com>
2018-12-27 16:16:32 +0000

    Few changes which might break things, who knows.

--- src/main/java/com/kieran/api/dao/queries/ProjectRepository.java
+++ src/main/java/com/kieran/api/dao/queries/ProjectRepository.java
@@ -1,14 +1,17 @@
 package com.kieran.api.dao.queries;
 
 import com.kieran.api.model.Project;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query(value = "SELECT * FROM projects", nativeQuery = true)
