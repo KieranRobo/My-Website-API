09e66916f6d6ed74b57de064097bf4a80df1f2a6
Kieran Robertson <kieran.robertson.2013@uni.strath.ac.uk>
2019-03-10 16:34:08 +0000

    Some initial structure for OAuth2

---
+++ src/main/java/com/kieran/api/configuration/OAuthConfguration.java
@@ -0,0 +1,7 @@
+package com.kieran.api.configuration;
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+public class OAuthConfguration extends AuthorizationServerConfigurerAdapter {
+    
+}
---
+++ src/main/java/com/kieran/api/security/ResourceServerConfiguration.java
@@ -0,0 +1,6 @@
+package com.kieran.api.security;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+
+public class ResourceServerConfiguration extends OAuth2ResourceServerAutoConfiguration {
+}
